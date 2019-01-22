cd4acc85c09721dfea3f32accb0e853a5931b14e
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2019-01-22 19:33:28 -0200

    Create all crawlers on initialization

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const projectListCrawler = require('./src/crawlers/project-list');
+const projectListCrawlerFactory = require('./src/crawlers/project-list');
+const peopleCrawlerConfiguration = require('./src/crawlers/configurations/people/people-configuration');
+const peopleCltCrawlerConfiguration = require('./src/crawlers/configurations/people/people-clt-configuration');
+const peopleCrawlerFactory = require('./src/crawlers/people');
 const { BASE_URL } = require('./src/constants');
+
+const peopleCrawler = peopleCrawlerFactory(peopleCrawlerConfiguration);
+const peopleCltCrawler = peopleCrawlerFactory(peopleCltCrawlerConfiguration);
+const projectListCrawler = projectListCrawlerFactory(peopleCrawler, peopleCltCrawler);
 
 projectListCrawler.queue(`${BASE_URL}/swfwfap151`);
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 module.exports = {
   BASE_URL: 'https://fap6.fapeu.org.br/scripts/fapeusite.pl',
-  BASE_CLT_URL: 'https://fap6.fapeu.org.br/scripts/fapeusite.pl/swfwfap060-28.p',
+  BASE_PEOPLE_URL: 'https://fap6.fapeu.org.br/scripts/fapeusite.pl/swfwfap060-24.p',
+  BASE_PEOPLE_CLT_URL: 'https://fap6.fapeu.org.br/scripts/fapeusite.pl/swfwfap060-28.p',
 };
